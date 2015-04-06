52dd4170db4e19c92ca52eddd5469dbb0934d913
pandafulmanda <pandafulmanda@gmail.com>
2015-04-06 12:07:16 -0500

    game play more like galaga

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,9 @@
 
 var spaceship = document.querySelector('#spaceship');
 var beam = document.querySelector('.beam');
-var shoot = false;
 
 function shootBeam(){
-    shoot = true;
-    animateBeam(0);
-}
-
-function stopBeam(){
-    shoot = false;
-}
-
-function animateBeam(i){
+    var i = 0;
     setTimeout(function(){
         beam.style.bottom = '800px';
     }, (i + 0.5) * 200);
@@ -26,9 +17,54 @@ function moveTo(position){
     spaceship.style.left = position.x + 'px';
 }
 
-document.addEventListener('mousemove', moveTo);
-spaceship.addEventListener('mousedown', shootBeam);
-spaceship.addEventListener('mouseup', stopBeam);
+function moveLeft(){
+    var currentPos = getPositionAsNumber(spaceship.style.left);
+    currentPos = currentPos - 50;
+    if(currentPos < 0){
+        return;
+    }
+    spaceship.style.left = currentPos + 'px';
+}
+
+function moveRight(){
+    var currentPos = getPositionAsNumber(spaceship.style.left);
+    currentPos = currentPos + 50;
+    if(currentPos > window.innerWidth){
+        return;
+    }    
+    spaceship.style.left = currentPos + 'px';
+}
+
+function getPositionAsNumber(position){
+    position = position? position : '50%';
+
+    var isPX = position.match('px')? true : false;
+    var isPercent = position.match('%')? true : false;
+    var percent, pixels;
+    if(isPX){
+        pixels = position.replace('px','');
+        return pixels * 1;
+    }
+    if(isPercent){
+        percent = position.replace('%','');
+        return (((percent/100) * window.innerWidth).toFixed(0)) * 1;
+    }
+}
+
+
+document.addEventListener('keydown', function(keyEvent){
+    if(keyEvent.keyCode == 32){
+        shootBeam()
+    }
+    if(keyEvent.keyCode == 37){
+        moveLeft();
+    }
+    if(keyEvent.keyCode == 39){
+        moveRight();
+    }
+});
+
+// document.addEventListener('mousemove', moveTo);
 spaceship.addEventListener('dragstart', function(dragEvent){
     dragEvent.preventDefault();
 });
--- curriculum/lesson-4.md
+++ curriculum/lesson-4.md
@@ -1,3 +1,7 @@
 # Lesson Four
 
 ## Events?
+
+1. Events
+1. If-else
+1. Switch
